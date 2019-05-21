773dd55a032e43afc74d4e3f55aa1bab1596a7cf
Shoxabbos <19941001@inbox.ru>
2019-05-21 15:06:08 +0500

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -18,57 +18,6 @@ class Plugin extends PluginBase
     }
 
     public function register() {
-    	 
-    	// Double event hook that affects user #2 only
-        Page::extend(function($model) {
-
-
-            $model->bindEvent('model.afterFetch', function() use ($model) {
-            	$model->addDynamicProperty('customfields', null);
-
-                $model->addDynamicMethod('getCustomfieldsAttribute', function() use ($model) {
-			        if (isset($this->customfields) && $this->customfields) {
-			            return $this->customfields;
-			        } else {
-                        $pageId = str_replace(".htm", "", $model->fileName);
-                        $group = Group::where('page', $pageId)->first();
-
-			            return $this->customfields = isset($group->properties) ? $group->properties : [];
-			        }
-			    });
-
-
-                $model->addDynamicMethod('getCustomfield', function($name) use ($model) {
-			        return $this->customfields;
-			    });
-
-                $model->addDynamicMethod('setCustomfield', function($name, $value) use ($model) {
-                    $pageId = str_replace(".htm", "", $model->fileName);
-                    $group = Group::where('page', $pageId)->first();
-
-                    if (!$group) {
-                        return;
-                    }
-
-                    return Property::where('group_id', $group->id)->where('name', $name)->update([
-                        'value' => $value
-                    ]);
-                });
-
-
-                $model->bindEvent('model.beforeSave', function() use ($model) {
-                    $customfields = $model->customfields;
-
-                    foreach (post('customfields') as $key => $value) {
-                        $model->setCustomfield($key, $value);                        
-                    }
-                });
-
-
-            });
-        });
-
-
     	\Event::listen('backend.form.extendFields', function ($widget) {
             if (!$widget->model instanceof \Cms\Classes\Page) {
                 return;
@@ -77,8 +26,11 @@ class Plugin extends PluginBase
             if (!($theme = Theme::getEditTheme())) {
                 throw new ApplicationException(Lang::get('cms::lang.theme.edit.not_found'));
             }
+
+            $pageId = str_replace(".htm", "", $widget->model->fileName);
+            $group = Group::where('page', $pageId)->first();
+            $customfields = isset($group->properties) ? $group->properties : [];
     	 	
-            $customfields = $widget->model->getCustomfieldsAttribute();
             if (empty($customfields)) {
                 return;
             }
@@ -86,19 +38,17 @@ class Plugin extends PluginBase
             $fields = [];
 
             foreach ($customfields as $key => $value) {
-                $fields["customfields[{$value->name}]"] = [
-                    'label'   => $value->label,
-                    'type'    => $value->type,
-                    'comment'     => $value->comment,
-                    'default'     => $value->default,
-                    'value'     => $value->value,
+                $fields["settings[custom_{$value->name}]"] = [
+                    'label'     => $value->label,
+                    'type'      => $value->type,
+                    'comment'   => $value->comment,
+                    'default'   => $value->default,
                 ];
             }
 
             $widget->addFields($fields, 'primary');
         });
 
-    	
 
     }
     
