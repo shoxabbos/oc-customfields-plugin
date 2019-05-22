c4d09067a49bc500ce6e14b1809e61b9ef43825e
Shoxabbos <19941001@inbox.ru>
2019-05-22 20:09:26 +0500

    Add group by tabs

--- Plugin.php
+++ Plugin.php
@@ -21,17 +21,16 @@ class Plugin extends PluginBase
         Page::extend(function($model) {
             $model->addDynamicMethod('getCustomfieldsAttribute', function() use ($model) {
                 $pageId = str_replace(".htm", "", $model->fileName);
-                $group = Group::where('page', $pageId)->first();
-                $customfields = isset($group->properties) ? $group->properties : [];
-                return $customfields;
+                return Group::where('page', $pageId)->get();
             });
-
 
             $model->bindEvent('model.afterFetch', function() use ($model) {
                 $translatable = $model->translatable;
 
-                foreach ($model->customfields as $key => $value) {
-                    $translatable[] = "custom_".$value->name;
+                foreach ($model->customfields as $value) {
+                    foreach ($value->properties as $property) {
+                        $translatable[] = $value->code."_".$property->name;
+                    }
                 }
 
                 $model->addDynamicProperty('translatable', $translatable);
@@ -55,13 +54,16 @@ class Plugin extends PluginBase
 
             $fields = [];
 
-            foreach ($customfields as $key => $value) {
-                $widget->tabs['fields']["settings[custom_{$value->name}]"] = [
-                    'label'     => $value->label,
-                    'type'      => $value->type,
-                    'comment'   => $value->comment,
-                    'default'   => $value->default,
-                ];
+            foreach ($customfields as $value) {
+                foreach ($value->properties as $property) {
+                    $widget->tabs['fields']["settings[{$value->code}_{$property->name}]"] = [
+                        'label'     => $property->label,
+                        'type'      => $property->type,
+                        'comment'   => $property->comment,
+                        'default'   => $property->default,
+                        'tab'       => $value->name
+                    ];
+                }
             }
         });
 
