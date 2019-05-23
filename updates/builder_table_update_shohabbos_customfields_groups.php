66ccabccbd4dc661a3710e11a7950f09380871ef
Shoxabbos <19941001@inbox.ru>
2019-05-23 10:15:07 +0500

    Remove description fields from groups

---
+++ updates/builder_table_update_shohabbos_customfields_groups.php
@@ -0,0 +1,23 @@
+<?php namespace Shohabbos\Customfields\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateShohabbosCustomfieldsGroups extends Migration
+{
+    public function up()
+    {
+        Schema::table('shohabbos_customfields_groups', function($table)
+        {
+            $table->dropColumn('description');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('shohabbos_customfields_groups', function($table)
+        {
+            $table->text('description')->nullable();
+        });
+    }
+}
