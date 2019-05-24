2e573dade2bf31b90924b15b5b16c1fe9744b022
Shoxabbos <19941001@inbox.ru>
2019-05-24 12:12:47 +0500

    Translate messages

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -18,4 +18,9 @@
         'name' => 'Название вкладки',
         'page' => 'Страница',
     ],
+    'permession' => [
+        'manage_properties' => 'Управление свойствами',
+        'manage_groups' => 'Управление группами',
+        'manager_cms_code_content' => 'Edit cms code content',
+    ],
 ];
