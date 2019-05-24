b9c77d9915aa520115ce115173b56993390dd4a5
Shoxabbos <19941001@inbox.ru>
2019-05-24 12:39:30 +0500

    Translate messages

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,9 +1,33 @@
 <?php return [
     'plugin' => [
         'name' => 'CustomFields',
-        'description' => 'This plugin allows add custom fields to CMS page'
+        'description' => 'This plugin allows add custom fields to CMS page',
     ],
     'editor' => [
-    	'custom_fields_tab' => 'Custom fields'
-    ]
+        'custom_fields_tab' => 'Custom fields',
+    ],
+    'property' => [
+        'type' => 'Type',
+        'comment' => 'Comment',
+        'default' => 'Default value',
+        'name' => 'Property name (ex: footer_text)',
+        'label' => 'Label',
+    ],
+    'group' => [
+        'is_repeater' => 'Is repeater',
+        'name' => 'Tab name',
+        'page' => 'Page',
+        'properties' => 'Fields',
+        'code' => 'Code',
+        'updated_at' => 'Updated at',
+        'created_at' => 'Created at',
+    ],
+    'permession' => [
+        'manage_properties' => 'Manage properties',
+        'manage_groups' => 'Manage groups',
+        'manager_cms_code_content' => 'Edit cms code content',
+    ],
+    'menu' => [
+        'groups' => 'Groups',
+    ],
 ];
